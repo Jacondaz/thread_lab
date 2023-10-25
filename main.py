57d9b73bcf4808bde4314fdfb7a824487dfd10fc
Nikita Karetnikov <NikitaKaretnikov@gmail.com>
2023-10-25 03:21:14 +0300

    add feature stop and fix bugs

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ class Memory:
         self.memory = 0
 
     def collect_values(self):
-        self.memory = psutil.virtual_memory().available / 1_048_576
-        print(self.memory)
-        self.draw_results()
-
-    def draw_results(self):
-        now = datetime.now()
-        with open("output.csv", 'a') as file:
-            csv.writer(file, lineterminator="\r").writerow([now.strftime("%H:%M:%S"), "Available memory", self.memory])
-        sleep(60)
-        self.collect_values()
+        while status:
+            self.memory = psutil.virtual_memory().available / 1_048_576
+            now = datetime.now()
+            with open("output.csv", 'a') as file:
+                csv.writer(file, lineterminator="\r").writerow([now.strftime("%H:%M:%S"), "Available memory",
+                                                                self.memory])
+            sleep(60)
 
 
 class Cpu_info:
@@ -28,30 +25,36 @@ class Cpu_info:
         self.cpu_percent = 0
 
     def collect_values(self):
-        self.count_process = len(list((psutil.process_iter())))  # количество запущенный процессов
-        self.cpu_percent = psutil.cpu_percent(interval=None, percpu=False)  # нагрузка цп в процентах
-        self.draw_results()
-
-    def draw_results(self):
-        now = datetime.now()
-        with open("output.csv", 'a') as file:
-            csv.writer(file, lineterminator="\r").writerows([[now.strftime("%H:%M:%S"), "Count process",
-                                                            self.count_process], [now.strftime("%H:%M:%S"), "Cpu load",
-                                                            self.cpu_percent]])
-        sleep(60)
-        self.draw_results()
+        while status:
+            self.count_process = len(list((psutil.process_iter())))  # количество запущенный процессов
+            self.cpu_percent = psutil.cpu_percent(interval=None, percpu=False)  # нагрузка цп в процентах
+            now = datetime.now()
+            with open("output.csv", 'a') as file:
+                csv.writer(file, lineterminator="\r").writerows([[now.strftime("%H:%M:%S"), "Count process",
+                                                                  self.count_process],
+                                                                 [now.strftime("%H:%M:%S"), "Cpu load",
+                                                                  self.cpu_percent]])
+            sleep(60)
 
 
 def main():
+    global status
     thread1 = Thread(target=Thread_CPU.collect_values, daemon=True)
     thread2 = Thread(target=Thread_Memory.collect_values, daemon=True)
     thread1.start()
     thread2.start()
+    print("Подсчёт метрик начался...")
+    while status:
+        temp = input("> Введите stop для остановки подсчёта метрик: ")
+        if temp == "stop":
+            status = False
+            print("Подсчёт метрик остановился")
     thread1.join()
     thread2.join()
 
 
 if __name__ == '__main__':
+    status = True
     Thread_CPU = Cpu_info()
     Thread_Memory = Memory()
     main()
